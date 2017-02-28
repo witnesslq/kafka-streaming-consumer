21c8d5933740e929ea370ece34fee4c6ac3dcbcd
duguyiren3476 <duguyiren3476@163.com>
2017-02-28 11:14:20 +0800

    add topic-app-data

--- src/main/scala/com/caishi/kafka/customer/Common.scala
+++ src/main/scala/com/caishi/kafka/customer/Common.scala
@@ -50,26 +50,36 @@ object Common {
     val km = new KafkaManager(kafkaParams)
     val data = km.createDirectStream[String, String, StringDecoder, StringDecoder](ssc,kafkaParams,topicsSet)
     // json to K/V
-    val lines = data.map(_._2).filter(!"".equals(_))
-    lines.filter(checkDataType2(_,"topic-user-info")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-info",pathPre))
-    lines.filter(checkDataType2(_,"topic-user-active")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-active",pathPre))
-    lines.filter(checkDataType2(_,"topic-user-course-subscribe")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-course-subscribe",pathPre))
-    lines.filter(checkDataType2(_,"topic-user-course-unsubscribe")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-course-unsubscribe",pathPre))
-    lines.filter(checkDataType2(_,"topic-user-workout-finished")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-workout-finished",pathPre))
-    lines.filter(checkDataType2(_,"topic-user-login")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-login",pathPre))
-    lines.filter(checkDataType2(_,"topic-user-openapp")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-openapp",pathPre))
-    lines.filter(checkDataType2(_,"topic-user-quitapp")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-quitapp",pathPre))
-    lines.filter(checkDataType2(_,"topic-user-browse")).map(Util.convertToJson(_,4)).foreachRDD(rdd => saveToParquet(rdd,"topic-user-browse",pathPre))
-    lines.filter(checkDataType2(_,"topic-user-media")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-media",pathPre))
-    lines.filter(checkDataType2(_,"topic-user-course-option")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-course-option",pathPre))
-    lines.filter(checkDataType2(_,"topic-user-search")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-search",pathPre))
-    lines.filter(checkDataType2(_,"topic-user-mealplan-checkin")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-mealplan-checkin",pathPre)) lines.filter(checkDataType2(_,"topic-user-mealplan")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-mealplan",pathPre))
+      try {
+          val lines = data.map(_._2).filter(!"".equals(_))
 
-    lines.filter(checkDataType2(_,"topic-mall-order")).map(Util.convertToJson(_,1)).foreachRDD(rdd => saveToParquet(rdd,"topic-mall-order",pathPre))
-    lines.filter(checkDataType2(_,"topic-mall-pay")).map(Util.convertToJson(_,2)).foreachRDD(rdd => saveToParquet(rdd,"topic-mall-pay",pathPre))
-    lines.filter(checkDataType2(_,"topic-mall-browse")).map(Util.convertToJson(_,3)).foreachRDD(rdd => saveToParquet(rdd,"topic-mall-browse",pathPre))
+          // 新的app数据不做分流,直接落入文件
+          lines.filter(checkNewDataType).foreachRDD(rdd => saveToParquet2(rdd,"topic-app-data",pathPre))
 
-//    lines.map(("",_)).groupByKey().foreachRDD()
+          lines.filter(checkDataType2(_,"topic-user-info")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-info",pathPre))
+          lines.filter(checkDataType2(_,"topic-user-active")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-active",pathPre))
+          lines.filter(checkDataType2(_,"topic-user-course-subscribe")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-course-subscribe",pathPre))
+          lines.filter(checkDataType2(_,"topic-user-course-unsubscribe")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-course-unsubscribe",pathPre))
+          lines.filter(checkDataType2(_,"topic-user-workout-finished")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-workout-finished",pathPre))
+          lines.filter(checkDataType2(_,"topic-user-login")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-login",pathPre))
+          lines.filter(checkDataType2(_,"topic-user-openapp")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-openapp",pathPre))
+          lines.filter(checkDataType2(_,"topic-user-quitapp")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-quitapp",pathPre))
+          lines.filter(checkDataType2(_,"topic-user-browse")).map(Util.convertToJson(_,4)).foreachRDD(rdd => saveToParquet(rdd,"topic-user-browse",pathPre))
+          lines.filter(checkDataType2(_,"topic-user-media")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-media",pathPre))
+          lines.filter(checkDataType2(_,"topic-user-course-option")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-course-option",pathPre))
+          lines.filter(checkDataType2(_,"topic-user-search")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-search",pathPre))
+          lines.filter(checkDataType2(_,"topic-user-mealplan-checkin")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-mealplan-checkin",pathPre))
+          lines.filter(checkDataType2(_,"topic-user-mealplan")).foreachRDD(rdd => saveToParquet(rdd,"topic-user-mealplan",pathPre))
+
+          lines.filter(checkDataType2(_,"topic-mall-order")).map(Util.convertToJson(_,1)).foreachRDD(rdd => saveToParquet(rdd,"topic-mall-order",pathPre))
+          lines.filter(checkDataType2(_,"topic-mall-pay")).map(Util.convertToJson(_,2)).foreachRDD(rdd => saveToParquet(rdd,"topic-mall-pay",pathPre))
+          lines.filter(checkDataType2(_,"topic-mall-browse")).map(Util.convertToJson(_,3)).foreachRDD(rdd => saveToParquet(rdd,"topic-mall-browse",pathPre))
+      }catch {
+          case ex: Exception =>{
+              println("project error:",ex.printStackTrace())
+          }
+      }
+
     //    更新kafka 监控offset值
     data.foreachRDD(rdd => {
       if(!rdd.isEmpty()){
@@ -82,11 +92,31 @@ object Common {
   }
 
 
-  def checkDataType2(d : String,dataType:String): Boolean ={
-    val s = com.alibaba.fastjson.JSON.parseObject(d).get("topic")
-    val isType = dataType.equals(s)
-    isType
-  }
+    def checkDataType2(d : String,dataType:String): Boolean ={
+        var isType = false
+        try{
+            val s = com.alibaba.fastjson.JSON.parseObject(d).get("topic")
+            isType = dataType.equals(s)
+        }catch {
+            case e: Throwable =>
+                println("ERROR: parse json string:\n" + e.toString + "\n")
+        }
+        isType
+    }
+    // 对app新版数据不进行分流,都放在一个目录下
+    def checkNewDataType(d : String): Boolean ={
+        var isType = false
+        try{
+            val s = com.alibaba.fastjson.JSON.parseObject(d).get("topic")
+
+            if (s != null)
+                isType = s.toString.toLowerCase().startsWith("topic-app-")
+        }catch {
+            case e: Throwable =>
+                println("ERROR: parse json string:\n" + e.toString + "\n")
+        }
+        isType
+    }
 
 //  def saveToParquet2(rdd: RDD[(String,Iterable[String])]): Unit = {
 //    val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
@@ -106,6 +136,23 @@ object Common {
 //  }
 
 
+    def saveToParquet2(rdd: RDD[String], dataType: String,pathPre : String): Unit = {
+        val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
+        sqlContext.setConf("parquet.enable.summary-metadata", "false")
+        import sqlContext.implicits._
+        val df = rdd.toDF("data")
+        // Loads an `JavaRDD[String]` storing JSON objects (one object per record)
+//        val df = sqlContext.read.json(rdd)
+        if(df.count() > 0){
+            val dirname = dirName.getDirName(pathPre,dataType)
+            try {
+                df.write.format("parquet").option("mergeSchema", "true").mode(SaveMode.Append).save(dirname)
+            }catch {
+                case e: Throwable =>
+                    println("ERROR: Save to parquet error\n" + e.toString + "\n" + rdd.collect())
+            }
+        }
+    }
   def saveToParquet(rdd: RDD[String], dataType: String,pathPre : String): Unit = {
     val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
     sqlContext.setConf("parquet.enable.summary-metadata", "false")
